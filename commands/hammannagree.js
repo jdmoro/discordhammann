9605040c0fcfc49ca352b00f1a943c062fc0e78a
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 01:07:10 +0800

    edit

--- commands/hammannagree.js
+++ commands/hammannagree.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  let replyroll = ["**ヘンタイ! はい ! フン！**\n*Pervert! Yes! Hmpf!", "**はい...な、何も言ってないわよ! このばか！! 早く仕事に戻るぞ！**\n*Ye... I-I didn't say anything! You Idiot!! Get back to work now!*"];
+  let replyroll = ["**ヘンタイ!!  はい!!  フン!！**\n*Pervert! Yes! Hmpf!*", "はい...な、何も言ってないわよ!\n*Ye... I-I didn't say anything! You Idiot!! Get back to work now!*"];
   let questionroll = args.slice(1).join(" ");
   let resultroll = Math.floor((Math.random() * replyroll.length));
 

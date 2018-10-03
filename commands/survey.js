cdfa19480571685fbb8c2c503586b2c8f9f8edd4
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 00:04:57 +0800

    args 1

--- commands/survey.js
+++ commands/survey.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  let survey = args[0];
+  let survey = args[1];
 
   message.delete().catch(O_o=>{});
   message.channel.send("Attention Perverted Commanders!\nThe higher ups has a message for all of you.\nDon't make me repeat myself?! Hmph!\n",survey);

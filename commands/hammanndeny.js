9d714f8969b135a352fecdd3ab06c9138b8e3563
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 00:35:09 +0800

    aaa

--- commands/hammanndeny.js
+++ commands/hammanndeny.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  if(!args[2]) return message.reply("Try to speak properly... baka! ");
   let replyroll = ["いいえ! いいえ!", "No! No! No!", "BAKA! NO!", "ほんとにばか！いいえ！いいえ！しないで!!!!"];
   let questionroll = args.slice(1).join(" ");
   let resultroll = Math.floor((Math.random() * replyroll.length));

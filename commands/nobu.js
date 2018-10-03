5d40054117b2be0fbdbc5444b8f8fd814dc35ea5
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 01:27:41 +0800

    nobuuu

---
+++ commands/nobu.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+  message.channel.send("Well, That is what </363857147584577538> would say.\n*Don't trust him! He is a Pervert!*")
+
+}
+
+module.exports.help = {
+  name: "nobu"
+}

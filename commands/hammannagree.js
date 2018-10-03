8550d6a8fd68362215262090b2e4fc248a9a6ae1
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 00:52:45 +0800

    hahaa

---
+++ commands/hammannagree.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+  let replyroll = [":hammanangry:", ":hammanangry:"];
+  let questionroll = args.slice(1).join(" ");
+  let resultroll = Math.floor((Math.random() * replyroll.length));
+
+  message.delete().catch(O_o=>{});
+  message.channel.send(replyroll[resultroll]);
+
+}
+
+module.exports.help = {
+  name: "agree"
+}

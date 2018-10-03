473be459236ade877167448e54ffed3f5864b179
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 07:14:37 +0800

    a

--- commands/ask.js
+++ commands/ask.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot,message,args) => {
     let replyroll = ["Yes!", "No! No! No!", "Maybe? I don’t know", "Hmph! I guess so... Baka!"];
     let whyroll = ["Because you're a pervert!", "ask yourself! idiot.. Hmpf!", "Sims would know..Hmpf!"];
     let howroll = ["How would I know?", "Buy more gems! Akashi?!", "By not being an idiot! Baka!", "Hammann won't answer stupid questions"];
+    let whenroll = ["How would I know?", "Hammann won't answer stupid questions"];
 
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * replyroll.length));
@@ -14,7 +15,7 @@ module.exports.run = async (bot,message,args) => {
     } else if (args[0] === 'how'){
       message.channel.send(howroll[resultroll]);
     } else if (args[0] === 'when'){
-       message.channel.send(howroll[resultroll]);
+       message.channel.send(whenroll[resultroll]);
     } else {
       message.channel.send(replyroll[resultroll]);
     }

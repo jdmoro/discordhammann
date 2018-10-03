34b48bd29d065697b48e6a8d85ef7caa39244b56
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 00:26:24 +0800

    commit

--- commands/survey.js
+++ commands/survey.js
@@ -5,8 +5,9 @@ module.exports.run = async (bot,message,args) => {
   let survey = args[0];
 
   message.delete().catch(O_o=>{});
-  message.channel.send("Attention Perverted Commanders!\nThe higher ups has a message for all of you.\nDon't make me repeat myself?! Hmph!\n")
+  message.channel.send("Attention Perverted Commanders!\nThe higher ups has a message for all of you.\nDon't make me repeat myself?! Hmph!\n");
   message.channel.send(survey);
+  message.channel.send("Please read see image below!");
 
 }
 

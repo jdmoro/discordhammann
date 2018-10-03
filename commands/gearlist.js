15fe672e0d3727d1dc5617a50fc82f7ef8aa00c0
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-03 23:48:05 +0800

    gearlist update

--- commands/gearlist.js
+++ commands/gearlist.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
   let gearlist = new Discord.RichEmbed()
   .setColor("#4aef5a")
   .addField("Gear/Equipment Tier List",
-  "[JP 2018-08-04](https://i.imgur.com/CDJQUzt.png)\n[CN 2018-05-02](https://i.imgur.com/gaP2MZd.png)\n[EN 2018-08-27](https://i.imgur.com/TiCGywy.jpg)\n\nPlease check [here](https://azurlane.koumakan.jp/Equipment_List) for full details on equipment statistics and [here](http://azurlane.koumakan.jp/Combat) if you want to know how it all fits together (heavy math warning). Also check out Your Waifu Is Ship's - what do I equip on this ship [guide](https://pastebin.com/3dMWR8nM).")
+  "[JP 2018-09-26](https://i.imgur.com/GbL9H4q.png)\n[JP 2018-08-04](https://i.imgur.com/CDJQUzt.png)\n[CN 2018-05-02](https://i.imgur.com/gaP2MZd.png)\n[EN 2018-08-27](https://i.imgur.com/TiCGywy.jpg)\n\nPlease check [here](https://azurlane.koumakan.jp/Equipment_List) for full details on equipment statistics and [here](http://azurlane.koumakan.jp/Combat) if you want to know how it all fits together (heavy math warning). Also check out Your Waifu Is Ship's - what do I equip on this ship [guide](https://pastebin.com/3dMWR8nM).\n Also for EN is [this](https://azurlane.koumakan.jp/User:Itsfyh/Equipment_Guide) detailed guide which is updated.")
 
   return message.channel.send(gearlist);
 }

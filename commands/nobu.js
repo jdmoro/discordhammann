78a38b8adf0aad6023056a8d2fe0ea0339f10b56
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 01:29:05 +0800

    edit

--- commands/nobu.js
+++ commands/nobu.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  message.channel.send("Well, That is what </363857147584577538> would say.\n*Don't trust him! He is a Pervert!*")
+  message.delete().catch(O_o=>{});
+  message.channel.send("Well, That is what </363857147584577538> would say.\n*Don't trust him! He is a Pervert!*");
 
 }
 

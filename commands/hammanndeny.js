df280faf35971372e80c9094c06432fa1d23bcc7
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 00:45:33 +0800

    bold fonts

--- commands/hammanndeny.js
+++ commands/hammanndeny.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  let replyroll = ["いいえ! いいえ!\n*NO! NO!*", "**ほんとにばか！ いいえ！ いいえ ！しないで !**\n*You're Stupid! NO! NO! Dont!*"];
+  let replyroll = ["**いいえ!! いいえ!!**\n*NO!! NO!!*", "**ほんとにばか！ いいえ！ いいえ ！しないで !**\n*You're Stupid! NO! NO! Dont!*"];
   let questionroll = args.slice(1).join(" ");
   let resultroll = Math.floor((Math.random() * replyroll.length));
 

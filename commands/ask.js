87a80599e52d1552138fe96a369466e76b74cf45
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 14:12:46 +0800

    as

--- commands/ask.js
+++ commands/ask.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot,message,args) => {
     let whenroll = ["How would I know?", "Hammann won't answer stupid questions", "When? when you give what Hammann wants!"];
     let whereroll = ["Eh?! I'm not a map!", "In the depths of the sea! where you will be if you don't give Hammann food!", "Do I look like I know?", "Well, I haven't heard a question as stupid as that."];
     let whatroll = ["I'm not answering a pervert!", "I don't know!", "*snobs*", "I don't care.", "Do you really think I'd answer you?", "*hmpf*"];
+    let whoroll = ["The Pervert", "That certain Admiral", "The Vice-Admirals of course", "Me!", "It was Sims!"];
 
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * replyroll.length));
@@ -26,6 +27,9 @@ module.exports.run = async (bot,message,args) => {
     }
     else if (args[0] === 'what'){
         return message.channel.send(whatroll[resultroll]);
+    }
+    else if (args[0] === 'who'){
+        return message.channel.send(whoroll[resultroll]);
     }
     else {
         return message.channel.send(replyroll[resultroll]);

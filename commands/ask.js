24f026fbdbabdd62e2b1339812e9134f3d662371
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-04 14:07:28 +0800

    ads

--- commands/ask.js
+++ commands/ask.js
@@ -6,8 +6,8 @@ module.exports.run = async (bot,message,args) => {
     let whyroll = ["Because you're a pervert!", "ask yourself! idiot.. Hmpf!", "Sims would know..Hmpf!"];
     let howroll = ["How would I know?", "Buy more gems! Akashi?!", "By not being an idiot! Baka!", "Hammann won't answer stupid questions"];
     let whenroll = ["How would I know?", "Hammann won't answer stupid questions", "When? when you give what Hammann wants!"];
-    let whereroll = ["Eh?! I'm not a map!", "In the depths of the sea! where you will be if you don't give Hammann food!", "Eh?! I'm not a map!", "In the depths of the sea! where you will be if you don't give Hammann food!"];
-    let whatroll = ["I'm not answering a pervert!", "I don't know!", "*snobs*", "I'm not answering a pervert!", "I don't know!", "*snobs*"];
+    let whereroll = ["Eh?! I'm not a map!", "In the depths of the sea! where you will be if you don't give Hammann food!", "Do I look like I know?", "Well, I haven't heard a question as stupid as that."];
+    let whatroll = ["I'm not answering a pervert!", "I don't know!", "*snobs*", "I don't care.", "Do you really think I'd answer you?", "*hmpf*"];
 
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * replyroll.length));

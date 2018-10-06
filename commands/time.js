7d60f50233ff9bff0f9461a00f0bb7a34a1b9226
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 05:04:29 +0800

    as

--- commands/time.js
+++ commands/time.js
@@ -2,13 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  var d = new Date,
-  let dformat = [d.getMonth(),
-         d.getDate(),
-         d.getFullYear()].join('/')+' '+
-        [d.getHours()-8,
-         d.getMinutes(),
-         d.getSeconds()].join(':');
+  var d = new Date;
+
+  let dformat = [d.getMonth(), d.getDate(), d.getFullYear()].join('/')+' '+ [d.getHours()-8, d.getMinutes(), d.getSeconds()].join(':');
 
   message.channel.send(dformat);
 

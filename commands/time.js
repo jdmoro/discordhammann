2d2d50a75243eac5c912d829220107e07debd9b0
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 05:23:01 +0800

    as

--- commands/time.js
+++ commands/time.js
@@ -3,6 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
 
+    let padLeft = function(base,chr){
+    var len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+  }
 
   var d = new Date;
 

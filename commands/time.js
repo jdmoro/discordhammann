999cd001e7fd8c4a0a335bcc62b498460157d985
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 05:13:20 +0800

    aasd

--- commands/time.js
+++ commands/time.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot,message,args) => {
 
   var d = new Date;
 
-  let dformat = [d.getMonth()+1, d.getDate(), d.getFullYear()].join('|')+'••'+ [d.getHours()-8, d.getMinutes(), d.getSeconds()].join(':');
+  let dformat = [d.getMonth()+1, d.getDate(), d.getFullYear()].join('|')+'\n'+ [d.getHours()-8, d.getMinutes(), d.getSeconds()].join(':');
 
-  message.channel.send("AzurLane's Server time is now: "+ dformat);
+  message.channel.send("AzurLane's Server time is now:\n"+ dformat);
 
 }
 

398c765084d423afba56946d8695c0619f7256d0
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 05:55:05 +0800

    as

--- commands/praise.js
+++ commands/praise.js
@@ -2,14 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-
-  let pic = message.channel.send("", {
+  message.channel.send(" ", {
     file: "https://media.giphy.com/media/40677cZyM0BLiLPpgr/giphy.gif"
   });
-  let count = [`${pic}`, "BINGO!"];
-  let roll = Math.floor((Math.random() * count.lenght));
-
-  message.channel.send(count[roll]);
 
 }
 

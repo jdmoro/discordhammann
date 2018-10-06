6cd2e4d3fcf6c2da10cac28d67e475707c1756fe
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 04:36:22 +0800

    aaa

---
+++ commands/time.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+  message.channel.send((new Date()).toString());
+
+}
+
+module.exports.help = {
+  name: "time"
+}

25f8f94e5cd9baef8ec5a4b644e7d65730358923
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-07 05:27:40 +0800

    asd

--- commands/time.js
+++ commands/time.js
@@ -2,9 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
+  Number.prototype.padLeft = function(base,chr){
+    var len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+  }
+
   var d = new Date;
 
-  let dformat = [d.getMonth()+1, d.getDate(), d.getFullYear()].join('/')+'\n'+ [d.getHours()-8, d.getMinutes(), d.getSeconds()].join(':');
+  let dformat = [d.getMonth()+1, d.getDate().padLeft(), d.getFullYear()].join('/')+'\n'+ [d.getHours()-8, d.getMinutes(), d.getSeconds()].join(':');
 
   message.channel.send("AzurLane's Server time is now:\n"+ dformat);
 

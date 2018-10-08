2a91da9cfd5f8c15475f91697bc7b961ac1e7b14
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-09 07:49:17 +0800

    as

--- commands/time.js
+++ commands/time.js
@@ -14,8 +14,7 @@ module.exports.run = async (bot,message,args) => {
 
 
 
-  message.channel.send("**AzurLane's Server Time:**");
-  message.channel.send(dformat);
+  message.channel.send("**AzurLane's Server Time:**\n" + dformat);
 
 }
 

28c1311de8db4324405516977dd2a4498c297fe6
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-09 07:26:33 +0800

    as

--- commands/time.js
+++ commands/time.js
@@ -11,7 +11,21 @@ module.exports.run = async (bot,message,args) => {
 
   let dformat = [(d.getMonth()+1).padLeft(), d.getDate().padLeft(), d.getFullYear()].join('/')+'\n'+ [(d.getHours()-7).padLeft(), d.getMinutes().padLeft(), d.getSeconds().padLeft()].join(':');
 
-  message.channel.send("AzurLane's Server time is now:\n"+ dformat + "\n-7 is  5pm\n-6 is 6pm\n-5 is 7pm\n-4 is 8pm\n-3 is 9pm\n-2 is 10pm\n-1 is 11pm\n0 is 12pm");
+
+  let timetable = new Discord.RichEmbed();
+  .setColor("#4aef5a")
+  .setTitle("AzurLane's Server time")
+  .addField(dformat)
+  .setFooter("-7 is 5pm\n-6 is 6pm\n-5 is 7pm\n-4 is 8pm\n-3 is 9pm\n-2 is 10pm\n-1 is 11pm\n0 is 12pm")
+
+
+
+
+
+
+
+
+  message.channel.send(timetable);
 
 }
 

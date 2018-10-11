2fc571bb753deb68793a422577038424943c4ad3
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-12 03:28:48 +0800

    as

--- commands/youtube.js
+++ commands/youtube.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
 
 //!kick @user reason
 
-  let search = args.join("%20").slice(22);
+  let search = args[0];
 
   message.channel.send(`https://www.youtube.com/results?search_query=${search}`)
 }

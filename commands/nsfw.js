1dec0eb10999f1855df57cfb2534a7a131a62021
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-23 00:33:40 +0800

    asd

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
   let tonsfw = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tonsfw) return message.channel.send("I couldn't find that pervert.");
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Hmph! You need **Admin privilege** to do this!");
-  let nsfwrole = message.guild.roles.find(role => role.name === "nsfw role");
+  let nsfwrole = message.guild.roles.find(role => role.name === "nsfw");
 
   await(tonsfw.addRole(nsfwrole.id));
   message.channel.send(`<@${tonsfw.id}> has been added to the pervert list!`);

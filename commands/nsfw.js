fcbc9dba102d144f0e6ae452edd7f3a9041f8735
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-23 00:29:35 +0800

    asd

---
+++ commands/nsfw.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+  let tonsfw = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tonsfw) return message.channel.send("I couldn't find that pervert.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Hmph! You need **Admin privilege** to do this!");
+  let nsfwrole = message.guild.roles.find(role => role.name === "nsfw role");
+
+  await(tonsfw.addRole(nsfwrole.id));
+  message.channel.send(`<@${tomute.id}> has been added to the pervert list!`);
+
+}
+
+module.exports.help = {
+  name: "nsfw"
+}

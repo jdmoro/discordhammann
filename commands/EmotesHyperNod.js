3f6a0a72e0fda2c4caf5eac88b55b68585810b9e
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 16:34:55 +0800

    as

--- commands/EmotesHyperNod.js
+++ commands/EmotesHyperNod.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
 message.channel.send(" ",{
-  file: "https://i.imgur.com/rncloZJ.gif"
+  file: "https://media.discordapp.net/attachments/460643534685470730/504572735121391616/hyper_nod.gif"
 });
 
 }
 
 module.exports.help = {
-  name: "nod"
+  name: "hypernod"
 }

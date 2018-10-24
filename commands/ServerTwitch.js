ac28a4c7725d1ee4dc2d9113a976a70777aab282
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 10:31:23 +0800

    asd

--- commands/ServerTwitch.js
+++ commands/ServerTwitch.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot,message,args) => {
   message.delete().catch(O_o=>{});
 
   let user = message.author.id;
-  let sMember = message.author;
+  let sMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let sChannel = message.guild.channels.find(role => role.name === "stream");
   let twitch = message.guild.roles.find(role => role.name === "Streamer");
 
-  if(sMember.roles.has(twitch.id)) {
+  if(message.member.has(twitch.id)) {
     sChannel.send(`<@${user}>\nis now live and streaming.\nhttps://twitch.tv/nobukun`);
   } else {
   send.message.channel("You don't have the streamer tag.");

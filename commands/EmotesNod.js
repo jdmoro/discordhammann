0a567a08e1322c070d0e07483c0f2cb4d83b27fb
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 16:22:15 +0800

    asaaas

---
+++ commands/EmotesNod.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+message.channel.send(" ",{
+  file: "blob:https://imgur.com/f7f4568d-6e55-41ca-9164-4f10a3d9c061"
+});
+
+}
+
+module.exports.help = {
+  name: "nod"
+}

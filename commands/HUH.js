3ae091a9792ca8783fbd4eefa7835f87269b45d3
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 16:05:52 +0800

    as

--- commands/HUH.js
+++ commands/HUH.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-message.channel..send(" ",{
+message.channel.send(" ",{
   file: "https://i.imgur.com/85uKxip.png"
 });
 

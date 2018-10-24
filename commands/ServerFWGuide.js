b5c5bf1b74cc557261a0e61f131e4a820922cb93
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 22:08:36 +0800

    a

--- commands/ServerFWGuide.js
+++ commands/ServerFWGuide.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
 message.channel.send(" ",{
-  file: "https://i.imgur.com/rncloZJ.gif"
+  file: "https://cdn.discordapp.com/attachments/503541610802446356/504039381426896898/unknown.png"
 });
 
 }
 
 module.exports.help = {
-  name: "nod"
+  name: "fw"
 }

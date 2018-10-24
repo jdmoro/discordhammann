4168f4ef6b093b852a84519a0851f662229267c8
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 10:45:51 +0800

    as

--- commands/ServerTwitch.js
+++ commands/ServerTwitch.js
@@ -6,7 +6,11 @@ module.exports.run = async (bot,message,args) => {
   let sChannel = message.guild.channels.find(role => role.name === "stream");
   let twitch = message.guild.roles.find(role => role.name === "Streamer");
 
-  sChannel.send(`<@${user}>\nis now live and streaming.\nhttps://twitch.tv/nobukun`);
+  if(message.author.id !== "363857147584577538") return send.message.channel("Nani?!");
+  else {
+      sChannel.send(`<@${user}>\nis now live and streaming.\nhttps://twitch.tv/nobukun`);
+  }
+
 
 }
 

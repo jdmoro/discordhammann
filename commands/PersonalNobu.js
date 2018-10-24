a52d6c44fc9b28de836ff0ee7fd2df2a4f2b3f5f
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 09:03:07 +0800

    as

--- commands/PersonalNobu.js
+++ commands/PersonalNobu.js
@@ -3,7 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
   message.delete().catch(O_o=>{});
-  message.channel.send("Well, That is what <@363857147584577538> would say.\n*Don't trust him! He is a Pervert!*");
+  if(message.author.id !== "363857147584577538") return send.message.channel("Don't spam Nobu's notifs! Hmpf!");
+  else {
+    message.channel.send("Well, That is what <@363857147584577538> would say.\n*Don't trust him! He is a Pervert!*");
+  }
 
 }
 

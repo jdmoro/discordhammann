2dc36367b10beadde61fab480ffbf10d4f48c22f
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 22:08:15 +0800

    s

---
+++ commands/ServerFWGuide.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+message.channel.send(" ",{
+  file: "https://i.imgur.com/rncloZJ.gif"
+});
+
+}
+
+module.exports.help = {
+  name: "nod"
+}

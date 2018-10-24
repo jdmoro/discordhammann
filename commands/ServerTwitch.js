6123e5561b377e863a90f8ce97470b34407df4ff
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-10-24 10:40:34 +0800

    asd

--- commands/ServerTwitch.js
+++ commands/ServerTwitch.js
@@ -3,12 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
   let user = message.author.id;
-  let sMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let sChannel = message.guild.channels.find(role => role.name === "stream");
   let twitch = message.guild.roles.find(role => role.name === "Streamer");
 
-  if(sMember.roles.has(twitch.id)) return sChannel.send(`<@${user}>\nis now live and streaming.\nhttps://twitch.tv/nobukun`);
-  if(!sMember.roles.has(twitch.id)) return send.message.channel("You don't have the streamer tag.");
+  if(message.author.has(twitch.id)) return sChannel.send(`<@${user}>\nis now live and streaming.\nhttps://twitch.tv/nobukun`);
+  if(message.author.has(twitch.id)) return send.message.channel("You don't have the streamer tag.");
 
 }
 

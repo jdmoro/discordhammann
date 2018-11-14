d3d24dc6eedcb5bcf2b1c6a889cae0376e8a5a2c
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-11-14 22:46:31 +0800

    sa

---
+++ commands/PersonalNobu2.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+  message.delete().catch(O_o=>{});
+  if(message.author.id !== "363857147584577538") return send.message.channel("Don't spam Nobu's notifs! Hmpf!");
+  else {
+    message.channel.send("Ehh?! Don't?! EHH??!! DON'T PUT THAT THING ANYWHERE NEAR ME!!/n IT WON'T FIT!! NO?!!");
+  }
+
+}
+
+module.exports.help = {
+  name: "nobu2"
+}

8490fa04a8152c032535f8fe48a0e37a0bf8a59d
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-11-15 05:48:21 +0800

    as

---
+++ commands/EmotesMutsuHammann.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+message.delete().catch(O_o=>{});
+message.channel.send(" ",{
+  file: "https://media.discordapp.net/attachments/504211156613988352/511951029009383426/1541888516763.jpg"
+});
+
+}
+
+module.exports.help = {
+  name: "nani?!"
+}

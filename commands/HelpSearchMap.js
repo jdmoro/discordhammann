d55d0b83f75af1c47cd1c3d892027b82b2b61d11
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-11-18 01:35:02 +0800

    asd

---
+++ commands/HelpSearchMap.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+//!kick @user reason
+
+  let search = args[0];
+
+  message.channel.send(`https://azurlane.koumakan.jp/${search}`)
+}
+
+module.exports.help = {
+  name: "map"
+}

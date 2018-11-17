d14a442d791117705d578b54f9936369012e8ab9
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-11-18 01:49:44 +0800

    asda

--- commands/EmotesHyperNod.js
+++ commands/EmotesHyperNod.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
+message.delete().catch(O_o=>{});
 message.channel.send(" ",{
   file: "https://media.discordapp.net/attachments/460643534685470730/504572735121391616/hyper_nod.gif"
 });
--- commands/EmotesWarshrug.js
+++ commands/EmotesWarshrug.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
+message.delete().catch(O_o=>{});
 message.channel.send(" ",{
   file: "https://i.imgur.com/5WMVvP4.gif"
 });

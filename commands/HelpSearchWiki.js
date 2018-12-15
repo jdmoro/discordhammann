906b12d787b25a03456546f2c4558e0ba260ebd8
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-12-16 01:01:42 +0800

    d

--- commands/HelpSearchWiki.js
+++ commands/HelpSearchWiki.js
@@ -5,10 +5,11 @@ module.exports.run = async (bot,message,args) => {
 //!kick @user reason
 
   let search = args[0];
+  if(!search) return message.channel.send("Input");
 
   message.channel.send(`https://azurlane.koumakan.jp/${search}`)
 }
 
 module.exports.help = {
-  name: "map"
+  name: "wiki"
 }

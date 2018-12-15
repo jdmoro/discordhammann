a37b601e2518fd66554f1095db71a06fa4883c87
jdmoro <43287309+jdmoro@users.noreply.github.com>
2018-12-16 01:12:38 +0800

    as

--- commands/HelpSearchWiki.js
+++ commands/HelpSearchWiki.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
 //!kick @user reason
 
   let search = args[0];
-  if(!search) return message.channel.send("```**Wiki Guide:**\nEvents - List of events\nCombat - All Combat related equations\nEquipment - Gears and etc.\nSkills - List of all known skills\nCommissions - commission schedules and drop list.");
+  if(!search) return message.channel.send("```*Please type !wiki keyword*\n**Wiki Guide:**\nEvents - List of events\nCombat - All Combat related equations\nEquipment - Gears and etc.\nSkills - List of all known skills\nCommissions - commission schedules and drop list.");
 
   message.channel.send(`https://azurlane.koumakan.jp/${search}`)
 }

80c4ecca72af43a2a735d16ac27b7ef35dc91ceb
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-01-27 11:36:48 +0800

    as

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
 
     let hitroll = ["**HIT!!**","**MISS!**","**CRITICAL HIT!!**","**HIT!**"];
     let questionroll = args.slice(1).join(" ");
-    let resultroll = Math.floor((Math.random() * replyroll.length));
+    let resultroll = Math.floor((Math.random() * hitroll.length));
 
     message.channel.send(hitroll[resultroll]);
   }

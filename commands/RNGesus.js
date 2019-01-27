a7399c4296fb7af04a24746013dc89acbb0e8d03
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-01-27 12:01:46 +0800

    as

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot,message,args) => {
     let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!user) return message.channel.send("I couldn't find the target.");
 
-    let hitroll = ["**HIT!!**","**MISS!**","**CRITICAL HIT!!**","**HIT!**"];
+    let hitroll = ["**HITS!!**","**MISSES!**","**CRITICALLY HITS!!**","**HITS!**"];
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * hitroll.length));
 
-    message.reply(hitroll[resultroll] + `${user}`);
+    message.reply(hitroll[resultroll] + ` ${user}`);
   }
 
 module.exports.help = {

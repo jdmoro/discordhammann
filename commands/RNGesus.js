28f99b4459ff709a9bd2de45cdcf6230faaf2df3
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-01-27 11:30:41 +0800

    as

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -3,12 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message,args) => {
 
     let hitroll = ["**HIT**","**MISS**","**CRITICAL HIT**","**HIT**"];
-
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * replyroll.length));
 
-    if(args[0] === 'enemy') {
-        return message.channel.send(hitroll[resultroll]);
+    message.channel.send(hitroll[resultroll]);
   }
 }
 module.exports.help = {

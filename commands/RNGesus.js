d2b92d5c99e0e6a725aeb01abced1737d985dbf1
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-01-27 11:54:12 +0800

    as

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -2,12 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!user) return message.channel.send("I couldn't find the target.");
+
     let hitroll = ["**HIT!!**","**MISS!**","**CRITICAL HIT!!**","**HIT!**"];
     let questionroll = args.slice(1).join(" ");
     let resultroll = Math.floor((Math.random() * hitroll.length));
 
-
-    message.reply(hitroll[resultroll]);
+    message.reply(hitroll[resultroll] + `${user}`);
   }
 
 module.exports.help = {

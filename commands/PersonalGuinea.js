19c7b10f10fbbddd9fe12a61eebda914caaa42d1
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-01-29 03:39:01 +0800

    Update PersonalGuinea.js

--- commands/PersonalGuinea.js
+++ commands/PersonalGuinea.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
+  message.delete().catch(O_o=>{});
   message.channel.send('Is <@477408453942312960> on the loose again?!\nThat weirdo needs to be put back into a lab!\nHe is a danger to everyone!\nI mean he was the first one to have @CANCER!!')
 
 }

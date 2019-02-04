0075b3ad244812b68880dabad4ad26c37cc0da2c
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-02-05 07:10:54 +0800

    as

--- commands/RPCountdown.js
+++ commands/RPCountdown.js
@@ -6,13 +6,14 @@ module.exports.run = async (bot,message,args) => {
   let tomute = message.author;
   let muterole = message.guild.roles.find(role => role.name === "commissioned");
 
-  let timer = args[1];
+  let timer = args[0];
   if(!timer) return message.reply("Specify a time.");
 
-  await();
+  await(tomute.addRole(muterole.id));
   message.channel.send(`countdown started!`);
 
   setTimeout(function(){
+    tomute.removeRole(muterole.id);
     message.channel.send(`<@${tomute.id}> countdown has finished!`);
     }, ms(timer));
 

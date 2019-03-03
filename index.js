4e84aa7672efc6134d4678141ef40269b0c1d6a9
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-03-03 19:26:15 +0800

    abc

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const token = process.env.token;
-const {
-  Client,
-  Attachment
-} = require('discord.js');
 const fs = require("fs");
 const bot = new Discord.Client({
   disabledEveryone: true
 });
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", (err, file) => {
+fs.readdir("./commands/", (err, files) => {
 
   if (err) console.log(err);
-
-  let jsfile = file.filter(f => f.split(".")
-    .pop() === "js")
+  let jsfile = files.filter(f => f.split(".")
+    .pop() === "js");
   if (jsfile.length <= 0) {
     console.log("Couldn't find commands.");
     return;
   }
 
   jsfile.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+    let pull = require(`./commands/${f}`);
+    bot.commands.set(pull.config.name, pull);
+    pull.config.aliases.forEach(alias => {
+      bot.aliases.set(alias, pull.config.name)
+    })
   });
-
 });
 
 bot.on("ready", async () => {
@@ -41,14 +37,17 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
 
   if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+  if (!message.guild) message.reply("STOOOOPP!!!");
 
   let prefix = botconfig.prefix;
+  if (!message.content.startsWith(prefix)) return;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if (cmd === "!") {
+    return;
+  }
+  let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
   if (commandfile) commandfile.run(bot, message, args);
 
 });

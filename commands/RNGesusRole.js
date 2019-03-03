d3a8a0733d26b6eef0a001ba54f3736221288f31
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-03-04 03:43:57 +0800

    abc

---
+++ commands/RNGesusRole.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let role = message.mentions.roles.first();
+  if (!user) return message.channel.send("I couldn't find the target.");
+  if (!role) return message.channel.send("Specify Role.");
+
+  if (message.author.id !== "363857147584577538") return;
+
+  role.members.forEach(member => {
+
+    let hitroll = ["**HITS!! <a:nani:503539484009168897>**", "**MISSES! <:haha:538469858338144278>**", "**CRITICALLY HITS!! <:cleve:503539339536629780>**", "**HITS!! <:heheh:551092454078545921>**"];
+    let questionroll = args.slice(1)
+      .join(" ");
+    let resultroll = Math.floor((Math.random() * hitroll.length));
+
+    message.reply(hitroll[resultroll] + ` ${user}`);
+  });
+}
+module.exports.help = {
+  name: "massattack"
+}

b8c43e72e15eb4997dddee14851a22b1ff014b85
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-03-03 22:06:32 +0800

    abc

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!user) return message.channel.send("I couldn't find the target.");
 
-  let hitroll = ["**HITS!! <a:nani:503539484009168897>**", "**MISSES! <a:LoliTriggered:529791443569475624>**", "**CRITICALLY HITS!! <:cleve:503539339536629780>**", "**HITS! <:heheh:551092454078545921>**"];
+  let hitroll = ["**HITS!! <a:nani:503539484009168897>**", "**MISSES! <a:LaffeyDrinking:529795381584855060>**", "**CRITICALLY HITS!! <:cleve:503539339536629780>**", "**HITS! <:heheh:551092454078545921>**"];
   let questionroll = args.slice(1)
     .join(" ");
   let resultroll = Math.floor((Math.random() * hitroll.length));

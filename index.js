1d9b5966d0ed46a4ad22d309721e8c42b09bcc8c
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-03-03 19:29:37 +0800

    abc

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const token = process.env.token;
+const {
+  Client,
+  Attachment
+} = require('discord.js');
 const fs = require("fs");
 const bot = new Discord.Client({
   disabledEveryone: true
 });
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir("./commands/", (err, file) => {
 
   if (err) console.log(err);
-  let jsfile = files.filter(f => f.split(".")
-    .pop() === "js");
+
+  let jsfile = file.filter(f => f.split(".")
+    .pop() === "js")
   if (jsfile.length <= 0) {
     console.log("Couldn't find commands.");
     return;
   }
 
   jsfile.forEach((f, i) => {
-    let pull = require(`./commands/${f}`);
-    bot.commands.set(pull.config.name, pull);
-    pull.config.aliases.forEach(alias => {
-      bot.aliases.set(alias, pull.config.name)
-    })
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
   });
+
 });
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is happy to serve the admiral`)
   bot.user.setActivity("Captain's Dorm", {
-    type: "Watching"
+    type: "Resting in"
   });
 
 });

7ff74cf61354a54d1dfbb4c4e9af023973480e9e
jdmoro <43287309+jdmoro@users.noreply.github.com>
2019-03-03 21:58:54 +0800

    abc

--- commands/RNGesus.js
+++ commands/RNGesus.js
@@ -1,16 +1,17 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot,message,args) => {
+module.exports.run = async (bot, message, args) => {
 
-    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!user) return message.channel.send("I couldn't find the target.");
+  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!user) return message.channel.send("I couldn't find the target.");
 
-    let hitroll = ["**HITS!!**","**MISSES!**","**CRITICALLY HITS!!**","**HITS!**"];
-    let questionroll = args.slice(1).join(" ");
-    let resultroll = Math.floor((Math.random() * hitroll.length));
+  let hitroll = ["**HITS!! <a:nani:503539484009168897>**", "**MISSES! <a:LoliTriggered:529791443569475624>**", "**CRITICALLY HITS!! <:cleve:503539339536629780>**", "**HITS! <:heheh:551092454078545921>**"];
+  let questionroll = args.slice(1)
+    .join(" ");
+  let resultroll = Math.floor((Math.random() * hitroll.length));
 
-    message.reply(hitroll[resultroll] + ` ${user}`);
-  }
+  message.reply(hitroll[resultroll] + ` ${user}`);
+}
 
 module.exports.help = {
   name: "attack"
